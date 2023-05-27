15675e3bb6d85a9c13fac43efdb460106f9a90d3
unknown <ido.bandd@gmail.com>
2023-05-27 23:32:44 +0300

    initial commit

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,12 +7,28 @@ module.exports = {
   ],
   theme: {
     extend: {
+      colors: {
+        dark: '#1b1b1b',
+        light: '#f5f5f5',
+        grayBorder: '#D3D3D3',
+        primary: '#B63E96',
+        secondary: '#58E6D9',
+        danger: 'rgb(190, 60, 60)',
+      },
       backgroundImage: {
         'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
         'gradient-conic':
           'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
       },
     },
+    screens: {
+      "2xl": { max: "1535px" },
+      "xl": { max: "1279px" },
+      "lg": { max: "1023px" },
+      "md": { max: "767px" },
+      "sm": { max: "639px" },
+      "xs": { max: "479px" },
+  },
   },
   plugins: [],
 }
